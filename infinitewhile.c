d700ce33eb84718f90ce8fb17f508493d81a87c0
PabloSiciliaAndrade <52049994+PabloSiciliaAndrade@users.noreply.github.com>
2019-10-10 11:37:37 -0500

    Create infinitewhile.c
    segundo archivo de la practica

---
+++ infinitewhile.c
@@ -0,0 +1,15 @@
+ #include <stdio.h> 
+ /*     Este programa genera un ciclo infinito. */ 
+ int main(){       
+ // Al igual que en la estructura if-else     
+ //                       0 -> falso     
+ // diferente de 0 -> verdadero           
+ // El siguiente es un ciclo infinito     
+ // porque la condición siempre es verdadera.     
+ // Así mismo, debido a que el ciclo consta de una sola línea, las     
+ // llaves { } son opcionales.           
+ 	while (100) {         
+ 		printf("Ciclo infinito.\nPara terminar el ciclo presione ctrl + c.\n");     
+ 	} 
+ 
+    return 0; } 
