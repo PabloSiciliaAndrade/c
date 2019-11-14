23eacd3f88f37ff0c6c7e75190c89090675e3ad0
Robin-Rojas <54251307+Robin-Rojas@users.noreply.github.com>
2019-11-14 17:24:03 -0600

    puse la función menú

--- proyecto final/polinox.c
+++ proyecto final/polinox.c
@@ -9,3 +9,10 @@ void leer_monomio_mayor(polinomio polinomio1){
 void imprimir_monomio_mayor(polinomio polinomio1){
 	printf("el polinomio es: %g %c^%d", polinomio1.monomio_mayor.coeficiente, polinomio1.monomio_mayor.variable, polinomio1.monomio_mayor.exponente );
 }
+void menu (){
+	printf("1: sumar polinomios \n");
+	printf("2: restar polinomios \n");
+	printf("3: multiplicar polinomios \n");
+	printf("4: dividir monomio entre polinomio \n");
+	printf("5: salir \n");
+}
