8360618419b8dc73116ba031a2c6917884651ba9
Robin-Rojas <54251307+Robin-Rojas@users.noreply.github.com>
2019-12-04 20:14:34 -0600

    Update main.c

--- proyecto final/main.c
+++ proyecto final/main.c
@@ -3,36 +3,29 @@
 #include "polinox.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-Polinomio polinomio1;
-polinomio1.monomio_mayor.coeficiente = 1;
-polinomio1.siguiente = NULL;
+//subida numero 2
+
 int main(int argc, char *argv[]) {
-	leer_monomio_mayor(polinomio1);
-	imprimir_monomio_mayor(polinomio1);
+	int Cantidad_de_polinomios = 0;
+	Polinomio *Lista[] = {(Polinomio*)malloc(sizeof(Polinomio))};
+
+
+	Cantidad_de_polinomios ++;
+	Lista[Cantidad_de_polinomios] = ((Polinomio*)malloc(sizeof(Polinomio)));
+	Lista[Cantidad_de_polinomios]->Monomio_menor = ((monomio*)malloc(sizeof(monomio))); 
+	//Polinomio*) realloc(Lista, Cantidad_de_polinomios * sizeof(Polinomio)
+ 	
+	//reservar_memoria(Lista->polinomio);
+	leer_polinomio(Lista[Cantidad_de_polinomios]);
+	ordenar(Lista[Cantidad_de_polinomios]);
 	
-	int op;
-	monomio polinomio1, polinomio2, monomio;
+	imprimir_polinomio(Lista[Cantidad_de_polinomios]);
+	//sumar_polinomios(polinomio1, polinomio2, polinomioR);
+	//imprimir_polinomio(polinomioR);
 
-	do 
-	{
-		menu();
-		scanf("%d", &op);
-
-		switch (op){
-			case 1:
-				suma = suma(polinomio1, polinomio2);
-				break;
-			case 2:
-				resta = resta(polinomio1, polinomio2);
-				break;
-			case 3:
-				multiplicaciOn = multiplicaciOn(polinomio1, polinomio2);
-				break;
-			case 4:
-				divisiOn = divisiOn(monomio, polinomio1);
-				break;
-		}
-	}while (op <5);
+	//freeMemory(polinomio1);
+	//freeMemory(polinomio2);
+	//freeMemory(polinomioR);
 
 	return 0;
 }
